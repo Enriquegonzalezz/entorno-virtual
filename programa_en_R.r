15c380b26507b558a8de49c3cadeb104e17e45ce
Enriquegonzalezz <enriquealfonzo123@gmail.com>
2024-04-26 13:09:48 -0400

    feat: upgrade programa_en_R.r

--- programa_en_R.r
+++ programa_en_R.r
@@ -1,8 +1,7 @@
 
 nombre <- readline(prompt="holiss como te llamas:" )
- numero1 <- readline(prompt="hola",nombre, "ahora dime un numero:")
- numero1 <- as.integer(numero1)
- numero2 <- readline(prompt="vale dime otro: ")
+  numero1 <- as.integer(readline(prompt=paste("Hola", nombre, "ahora dime un número: ")))
+ numero2 <- as.integer(readline(prompt=paste("vale ahora necesito otro: ")))
  numero2 <- as.integer(numero2)
  resultado <- numero1 + numero2
- print(paste(resultado))
+ print(paste("la suma de ellas: ", resultado))
